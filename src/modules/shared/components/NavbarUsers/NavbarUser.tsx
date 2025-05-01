ca13a5b7d7f345852d52a060918bbd2933851a7c
ahmedh997 <islamamer2211@gmail.com>
2025-05-01 21:56:22 +0300

    Adding-favorites-component

--- src/modules/shared/components/NavbarUsers/NavbarUser.tsx
+++ src/modules/shared/components/NavbarUsers/NavbarUser.tsx
@@ -1,4 +1,3 @@
-
 // import React, { useState } from 'react'
 // import AppBar from '@mui/material/AppBar';
 // import Box from '@mui/material/Box';
@@ -20,19 +19,18 @@
 //   const [active, setActive] = useState('home')
 //   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
 //   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  
-    
+
 //   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
 //     setAnchorElNav(event.currentTarget);
 //   };
 //   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
 //     setAnchorElUser(event.currentTarget);
 //   };
-  
+
 //   const handleCloseNavMenu = () => {
 //     setAnchorElNav(null);
 //   };
-  
+
 //   const handleCloseUserMenu = () => {
 //     setAnchorElUser(null);
 //   };
@@ -192,73 +190,85 @@
 //   )
 // }
 
-
-/**///////////////////////////////////// */
+/**/ //////////////////////////////////// */
 
 import React, { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import MenuItem from '@mui/material/MenuItem';
-import { Link, useNavigate } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import Button from '@mui/material/Button'; 
-import { useAuthContext } from '../../../../context/AuthContext';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import MenuItem from '@mui/material/MenuItem'
+import { Link, useNavigate } from 'react-router-dom'
+import Avatar from '@mui/material/Avatar'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import { useAuthContext } from '../../../../context/AuthContext'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 export default function NavbarUser() {
-  const { profileImage,userName } = useAuthContext();
-  const navigate = useNavigate();
-  const [active, setActive] = useState('home');
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const { profileImage, userName } = useAuthContext()
+  const navigate = useNavigate()
+  const [active, setActive] = useState('home')
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
 
-  const token = localStorage.getItem('token'); 
+  const token = localStorage.getItem('token')
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    setAnchorElNav(event.currentTarget)
+  }
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    setAnchorElUser(event.currentTarget)
+  }
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    setAnchorElNav(null)
+  }
   const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    setAnchorElUser(null)
+  }
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  };
+    localStorage.removeItem('token')
+    navigate('/')
+  }
 
   return (
-    <AppBar className='UserNav' position="static" sx={{ backgroundColor: '#ffffff', position: 'sticky', top: 0, zIndex: 1000 }}>
+    <AppBar
+      className="UserNav"
+      position="static"
+      sx={{
+        backgroundColor: '#ffffff',
+        position: 'sticky',
+        top: 0,
+        zIndex: 1000,
+      }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-
           {/* Logo */}
           <Box sx={{ marginLeft: { lg: '150px', sm: '0' } }}>
-            
-          <Typography
-            variant="h5"
-            sx={{
-              color: '#3252DF',
-              fontWeight: 500,
-            }}
-          >
-            Stay<span style={{ color: 'black' }}>cation.</span>
-          </Typography>
+            <Typography
+              variant="h5"
+              sx={{
+                color: '#3252DF',
+                fontWeight: 500,
+              }}
+            >
+              Stay<span style={{ color: 'black' }}>cation.</span>
+            </Typography>
           </Box>
 
           {/* Mobile Menu */}
-          <Box sx={{ flexGrow: 1, justifyContent: 'end', display: { xs: 'flex', md: 'none' } }}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'end',
+              display: { xs: 'flex', md: 'none' },
+            }}
+          >
             <IconButton size="large" onClick={handleOpenNavMenu}>
               <MenuIcon />
             </IconButton>
@@ -271,27 +281,47 @@ export default function NavbarUser() {
               transformOrigin={{ vertical: 'top', horizontal: 'left' }}
             >
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to='/' style={{ textDecoration: 'none' ,color:"#3252DF"}}>Home</Link>
+                <Link
+                  to="/"
+                  style={{ textDecoration: 'none', color: '#3252DF' }}
+                >
+                  Home
+                </Link>
               </MenuItem>
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to='/explore-all-rooms' style={{ textDecoration: 'none',color:"#152C5B" }}>Explore</Link>
+                <Link
+                  to="/explore-all-rooms"
+                  style={{ textDecoration: 'none', color: '#152C5B' }}
+                >
+                  Explore
+                </Link>
               </MenuItem>
 
               {token ? (
                 <>
                   <MenuItem onClick={handleCloseNavMenu}>
-                    <Link to='/' style={{ textDecoration: 'none',color:"#152C5B" }}>Reviews</Link>
+                    <Link
+                      to="/"
+                      style={{ textDecoration: 'none', color: '#152C5B' }}
+                    >
+                      Reviews
+                    </Link>
                   </MenuItem>
                   <MenuItem onClick={handleCloseNavMenu}>
-                    <Link to='/favorites' style={{ textDecoration: 'none',color:"#152C5B" }}>Favorites</Link>
+                    <Link
+                      to="/favorites"
+                      style={{ textDecoration: 'none', color: '#152C5B' }}
+                    >
+                      Favorites
+                    </Link>
                   </MenuItem>
                   <MenuItem onClick={handleCloseNavMenu}>
-                    
-        <Box sx={{ display: "flex", alignItems: "center"}}>
-          <Avatar alt="User Photo" src={profileImage} />
-          <Typography sx={{ ml: 1, color: "black" }}>{userName}</Typography>
-        </Box>
-                    
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <Avatar alt="User Photo" src={profileImage} />
+                      <Typography sx={{ ml: 1, color: 'black' }}>
+                        {userName}
+                      </Typography>
+                    </Box>
                   </MenuItem>
                   <MenuItem onClick={handleLogout}>
                     <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
@@ -300,12 +330,24 @@ export default function NavbarUser() {
               ) : (
                 <>
                   <MenuItem onClick={handleCloseNavMenu}>
-                    <Button component={Link} to="/auth/register" variant="contained" color="primary" fullWidth>
+                    <Button
+                      component={Link}
+                      to="/auth/register"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                    >
                       Register
                     </Button>
                   </MenuItem>
                   <MenuItem onClick={handleCloseNavMenu}>
-                    <Button component={Link} to="/auth/login" variant="contained" color="primary" fullWidth>
+                    <Button
+                      component={Link}
+                      to="/auth/login"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                    >
                       Login Now
                     </Button>
                   </MenuItem>
@@ -315,65 +357,107 @@ export default function NavbarUser() {
           </Box>
 
           {/* Desktop Menu */}
-          <Box sx={{ flexGrow: 1, justifyContent: 'end', alignItems: 'center', marginInline: '40px', display: { xs: 'none', md: 'flex' }, gap: 2 }}>
-            <Link to='/' onClick={() => setActive('home')} className={`link ${active === 'home' ? 'active' : ''}`} style={{textDecoration:"none",color:"#3252DF"}}>Home</Link>
-            <Link to='/explore-all-rooms' onClick={() => setActive('explore')} className={`link ${active === 'explore' ? 'active' : ''}`} style={{textDecoration:"none",color:"#152C5B"}}>Explore</Link>
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'end',
+              alignItems: 'center',
+              marginInline: '40px',
+              display: { xs: 'none', md: 'flex' },
+              gap: 2,
+            }}
+          >
+            <Link
+              to="/"
+              onClick={() => setActive('home')}
+              className={`link ${active === 'home' ? 'active' : ''}`}
+              style={{ textDecoration: 'none', color: '#3252DF' }}
+            >
+              Home
+            </Link>
+            <Link
+              to="/explore-all-rooms"
+              onClick={() => setActive('explore')}
+              className={`link ${active === 'explore' ? 'active' : ''}`}
+              style={{ textDecoration: 'none', color: '#152C5B' }}
+            >
+              Explore
+            </Link>
 
             {token ? (
               <>
-                <Link to='/' onClick={() => setActive('reviews')} className={`link ${active === 'reviews' ? 'active' : ''}`} style={{textDecoration:"none",color:"#152C5B"}}>Reviews</Link>
-                <Link to='/favorites' onClick={() => setActive('favorites')} className={`link ${active === 'favorites' ? 'active' : ''}`} style={{textDecoration:"none",color:"#152C5B"}}>Favorites</Link>
+                <Link
+                  to="/"
+                  onClick={() => setActive('reviews')}
+                  className={`link ${active === 'reviews' ? 'active' : ''}`}
+                  style={{ textDecoration: 'none', color: '#152C5B' }}
+                >
+                  Reviews
+                </Link>
+                <Link
+                  to="/favorites"
+                  onClick={() => setActive('favorites')}
+                  className={`link ${active === 'favorites' ? 'active' : ''}`}
+                  style={{ textDecoration: 'none', color: '#152C5B' }}
+                >
+                  Favorites
+                </Link>
 
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                <Box
+                  sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}
+                >
                   <Tooltip title="Open settings">
                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                        <Avatar alt="User" src={profileImage} />
-                      <Typography sx={{ ml: 1, color: "black" }}>{userName}</Typography>
+                      <Avatar alt="User" src={profileImage} />
+                      <Typography sx={{ ml: 1, color: 'black' }}>
+                        {userName}
+                      </Typography>
                       <ExpandMoreIcon sx={{ color: 'black', ml: 0.5 }} />
                     </IconButton>
                   </Tooltip>
                   <Menu
-    anchorEl={anchorElUser}
-    open={Boolean(anchorElUser)}
-    onClose={handleCloseUserMenu}
-    sx={{ mt: '45px' }}
-    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-  >
-    <MenuItem onClick={handleLogout}>
-      <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
-    </MenuItem>
-  </Menu>
+                    anchorEl={anchorElUser}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                    sx={{ mt: '45px' }}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                  >
+                    <MenuItem onClick={handleLogout}>
+                      <Typography sx={{ textAlign: 'center' }}>
+                        Logout
+                      </Typography>
+                    </MenuItem>
+                  </Menu>
                 </Box>
               </>
             ) : (
               <>
-              <Button 
-                component={Link} 
-                to="/auth/register" 
-                variant="contained" 
-                sx={{ 
-                  ml: 2, 
-                  backgroundColor: '#3252DF',
-                  '&:hover': { backgroundColor: '#2233aa' } 
-                }}
-              >
-                Register
-              </Button>
+                <Button
+                  component={Link}
+                  to="/auth/register"
+                  variant="contained"
+                  sx={{
+                    ml: 2,
+                    backgroundColor: '#3252DF',
+                    '&:hover': { backgroundColor: '#2233aa' },
+                  }}
+                >
+                  Register
+                </Button>
 
-<Button 
-  component={Link} 
-  to="/auth/login" 
-  variant="contained" 
-  sx={{ 
-    ml: 2, 
-    backgroundColor: '#3252DF',
-    '&:hover': { backgroundColor: '#2233aa' } 
-  }}
->
-  Login Now
-</Button>
-
+                <Button
+                  component={Link}
+                  to="/auth/login"
+                  variant="contained"
+                  sx={{
+                    ml: 2,
+                    backgroundColor: '#3252DF',
+                    '&:hover': { backgroundColor: '#2233aa' },
+                  }}
+                >
+                  Login Now
+                </Button>
               </>
             )}
           </Box>
